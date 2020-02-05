ed9863aafee000b6b9acb395f0853711b0c39dde
valentinb <valentin.bus@d2si.io>
2020-02-05 17:11:47 +0100

    [env] add os env

--- flask/app.py
+++ flask/app.py
@@ -3,6 +3,7 @@ To authent user have to go on /authent
 """
 import json
 import logging
+import os
 from flask import (
     Flask,
     request,
@@ -99,7 +100,5 @@ def get_cache():
 if __name__ == '__main__':
     app.run(
         debug=True,
-        host='0.0.0.0',
-        port=80,
-        #ssl_context=('cert.pem', 'key.pem')
+        host=os.getenv('HOME_URL'),
     )
