f9c06c749a82e8196e8356ab18c17dca76529c3a
valentinb <valentin.bus@d2si.io>
2020-02-12 14:39:15 +0100

    [api] rewrite route for swagger ui

--- flask/app.py
+++ flask/app.py
@@ -16,6 +16,7 @@ from flask import (
 from backend.spotify import Spotify
 from flask_cors import CORS
 from flask_caching import Cache
+from flask_restplus import Api, Resource
 from functools import wraps
 
 from pprint import pprint
@@ -25,6 +26,8 @@ logging.basicConfig(level=logging.DEBUG)
 spotify = Spotify()
 
 app = Flask(__name__)
+api = Api(app)
+
 CORS(app)
 app.config['SECRET_KEY'] = "secret_keyoidozinfoinoqifnoeinosifn"
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
@@ -55,46 +58,53 @@ def valid_token(f):
             return redirect('/authent')
     return wrap
 
-@app.route('/', methods=["GET"])
-def home():
-    return Response("Bienvenue")
+
+@api.route('/home')
+class home(Resource):
+    def get(self):
+        return Response("Bienvenue")
 
 
-@app.route('/authent', methods=["GET"])
-def authent():
-    return redirect(spotify._authorization_ulr())
+@api.route('/authent')
+class authent(Resource):
+    def get(self):
+        return redirect(spotify._authorization_ulr())
 
 
-@app.route('/get-token', methods=["GET"])
-def get_token():
-    code = request.args.get('code')
-    spotify._get_baerer_token(code)
-    session['baerer_token'] = f'Bearer {spotify.baerer_token}'
-    return Response('Vous êtes connecté')
+@api.route('/get-token')
+class GetToken(Resource):
+    def get(self):
+        code = request.args.get('code')
+        spotify._get_baerer_token(code)
+        session['baerer_token'] = f'Bearer {spotify.baerer_token}'
+        return Response('Vous êtes connecté')
 
 
-@app.route('/get-user', methods=["GET"])
 @valid_token
-def get_user():
-    user_information = spotify.get_user(
-        session.get('baerer_token')
-    )
-    return jsonify(user_information)
+@api.route('/get-user')
+class ValidToken(Resource):
+    def get(self):
+        user_information = spotify.get_user(
+            session.get('baerer_token')
+        )
+        return jsonify(user_information)
 
 
-@app.route('/get-tracks', methods=["GET"])
 @cache.cached(timeout=1000)
 @valid_token
-def get_tracks():
-    tracks = spotify.get_tracks(session.get('baerer_token'))
-    return jsonify(tracks)
+@api.route('/get-tracks')
+class GetTracks(Resource):
+    def get(self):
+        tracks = spotify.get_tracks(session.get('baerer_token'))
+        return jsonify(tracks)
 
 
-@app.route('/get-cache', methods=["GET"])
 @cache.cached(timeout=1000)
 @valid_token
-def get_cache():
-    return get_tracks()
+@api.route('/get-cache', methods=["GET"])
+class GetCache(Resource):
+    def get(self):
+        return get_tracks()
 
 
 if __name__ == '__main__':
