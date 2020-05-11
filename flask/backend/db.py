4dbd2cb8a5e9c327bd1c0d6ffe66d473d441b6c6
Valentin BUS <valentinoiho@gmail.com>
2020-05-11 10:45:35 +0200

    [db] add first db features

--- flask/backend/db.py
+++ flask/backend/db.py
@@ -4,18 +4,35 @@ from sqlalchemy import orm
 
 '''
 We have to use ORM like django project to create db's tables etc...
+Define a
 '''
 
-print('okkkkk')
+DB_PASSWORD=os.getenv('DB_PASSWORD')
 
-conn = psycopg2.connect(
-    host="localhost",
-    port=5432,
-    database="spotify_autom_db", 
-    user="valentinbus", 
-    password=os.getenv('DB_PASSWORD'),
-    connect_timeout=3
-)
+class Db:
+    def __init__(self):
+        self.host = 'localhost'
+        self.port = 5432
+        self.database="spotify_autom_db"
+        self.user="valentinbus"
+        self.password=DB_PASSWORD
+        self.connect_timeout=3
 
-print(conn)
+    def _connection(self):
+        connection = psycopg2.connect(
+            host=self.host,
+            port=self.port,
+            database=self.database, 
+            user=self.user, 
+            password=self.password,
+            connect_timeout=self.connect_timeout
+        )
+        self.connection = connection
+        print(connection)
 
+        return "Connection done"
+
+
+    
+db = Db()
+db._connection()
