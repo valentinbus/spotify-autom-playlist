00cdf30700b5c377ff8df98433a8a17e089f5221
Valentin BUS <valentinoiho@gmail.com>
2020-05-18 12:42:03 +0200

    [front] presentation update

---
+++ website/src/components/Test.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+    <footer class="footer text-center">
+        <div class="container">
+            <div class="row">
+                <!-- Footer Location-->
+                <div class="col-lg-4 mb-5 mb-lg-0">
+                    <h4 class="mb-4">LOCATION</h4>
+                    <p class="pre-wrap lead mb-0">2215 John Daniel DriveClark, MO 65243</p>
+                </div>
+                <!-- Footer Social Icons-->
+                <div class="col-lg-4 mb-5 mb-lg-0">
+                    <h4 class="mb-4">AROUND THE WEB</h4><a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/StartBootstrap"><i class="fab fa-fw fa-facebook-f"></i></a><a class="btn btn-outline-light btn-social mx-1" href="https://www.twitter.com/sbootstrap"><i class="fab fa-fw fa-twitter"></i></a><a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/startbootstrap"><i class="fab fa-fw fa-linkedin-in"></i></a><a class="btn btn-outline-light btn-social mx-1" href="https://www.dribble.com/startbootstrap"><i class="fab fa-fw fa-dribbble"></i></a>
+                </div>
+                <!-- Footer About Text-->
+                <div class="col-lg-4">
+                    <h4 class="mb-4">ABOUT FREELANCER</h4>
+                    <p class="pre-wrap lead mb-0">Freelance is a free to use, MIT licensed Bootstrap theme created by Start Bootstrap</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+        <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+@import "../css/body.css";
+@import "../css/heading.css";
+@import "../css/styles.css";
+</style>
--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,24 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Presentation from '@/components/Presentation'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
       path: '/presentation',
       name: 'Presentation',
-      meta: {layout: 'default'},
-      components: Presentation.default
+      component: Presentation
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
     },
     {
       path: '/header',
