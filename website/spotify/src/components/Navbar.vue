9d615351de56562790f58e020df82be44d14ce35
Valentin BUS <valentinoiho@gmail.com>
2020-05-18 18:24:29 +0200

    [vuetify] add

---
+++ website/spotify/src/components/Navbar.vue
@@ -0,0 +1,74 @@
+<template>
+<nav>
+  <v-toolbar dense>
+    <v-toolbar-title class='text-uppercase grey--text text-bold'>
+      <span class='font-weight-light'>
+        Spotify
+      </span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text color='grey'>
+      <span>Créer une playlist</span>
+    </v-btn>
+  </v-toolbar>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader'
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify'
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+      }
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com'
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com'
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify'
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs'
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify'
+      }
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer'
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined'
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+      }
+    ]
+  })
+}
+</script>
