b0948aef7fc3afe051177260ba5b33ff0d950258
Valentin BUS <valentinoiho@gmail.com>
2020-05-20 11:27:03 +0200

    [presentation] add card

---
+++ website/todo-ninja/src/router.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Dashboard from './views/Dashboard.vue'
+import Projects from './views/Projects.vue'
+import Team from './views/Team.vue'
+import Presentation from './views/Presentation'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: Projects
+    },
+    {
+      path: '/team',
+      name: 'team',
+      component: Team
+    },
+    {
+      path: '/presentation',
+      name: 'presentation',
+      component: Presentation
+    }
+
+  ]
+})
---
+++ website/todo-ninja/src/views/Presentation.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-container class="pa-4 text-center">
+    <h1 class="grey--text m-12">Selectionnez la playlist que vous souhaitez créer</h1>
+    {{ info }}
+    <v-row class="fill-height" align="center" justify="center">
+      <template v-for="(item, i) in items">
+        <v-col
+          :key="i"
+          cols="12"
+          md="4"
+        >
+          <v-hover>
+            <v-card
+              slot-scope="{ hover }"
+              :elevation="hover ? 12 : 2"
+              :class="{ 'on-hover': hover }"
+            >
+              <v-img
+                :src="item.img"
+                height="225px"
+              >
+                <v-card-title class="title white--text">
+                  <v-row
+                    class="fill-height flex-column"
+                    justify="space-between"
+                  >
+                    <p class="mt-4 subheading text-left">{{ item.title }}</p>
+
+                    <div>
+                      <p class="ma-0 body-1 font-weight-bold font-italic text-left">
+                        {{ item.text }}
+                      </p>
+                      <p class="caption font-weight-medium font-italic text-left">
+                        {{ item.subtext }}
+                      </p>
+                    </div>
+                  </v-row>
+                </v-card-title>
+                <div class="align-self-center">
+                    <v-button text 
+                    class="{ 'show-btns': hover }"
+                    color='transparent'
+                    >
+                        <v-icon
+                        :class="{ 'show-btns': hover }"
+                        color='transparent'>
+                            add
+                        </v-icon>
+                    </v-button>
+                </div>
+              </v-img>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from '../../node_modules/axios'
+
+  export default {
+    data: () => ({
+      name:'Presentation',
+      icons: ['mdi-rewind', 'mdi-play', 'mdi-fast-forward'],
+      info: null,
+      items: [
+        {
+          title: 'New Releases',
+          text: "It's New Release Friday",
+          subtext: 'Newly released songs. Updated daily.',
+          img: 'http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg',
+        },
+        {
+          title: 'Rock',
+          text: 'Greatest Rock Hits',
+          subtext: 'Lose yourself in rock tunes.',
+          img: 'https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+        },
+        {
+          title: 'Mellow Moods',
+          text: 'Ambient Bass',
+          subtext: 'Chill beats to mellow you out.',
+          img: 'http://lorempixel.com/output/abstract-q-c-640-480-6.jpg',
+        },
+      ],
+      transparent: 'rgba(255, 255, 255, 0)',
+    }),
+    mounted() {
+    axios
+    .get('http://localhost:5000/get-suggest-playlist')
+    .then(response => (this.info = response))
+  }
+  }
+</script>
+
+<style scoped>
+.v-card {
+  transition: opacity .4s ease-in-out;
+}
+
+.v-card:not(.on-hover) {
+  opacity: 0.6;
+ }
+
+.show-btns {
+  color: rgba(255, 255, 255, 1) !important;
+}
+</style>
