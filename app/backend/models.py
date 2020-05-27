9ec3f2af4f0a6fd9bbc406f2e5caf6208cad9f7d
Valentin BUS <valentinoiho@gmail.com>
2020-05-27 16:57:58 +0200

    [backend][models] add user_id fk in category

--- app/backend/models.py
+++ app/backend/models.py
@@ -37,6 +37,7 @@ class Track(db.Model):
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140))
+    user_id = db.Column(db.String(64), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Category {}>'.format(self.name)
