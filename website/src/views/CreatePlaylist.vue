485331fd091945e5f7927b7f8ce3e0969d4c085d
Valentin BUS <valentinoiho@gmail.com>
2020-05-29 11:41:57 +0200

    [frontend] create playlist is ok

--- website/src/views/CreatePlaylist.vue
+++ website/src/views/CreatePlaylist.vue
@@ -1,76 +1,86 @@
 <template>
-<div>
-    <v-container class="pa-4 text-center" v-if="this.$store.state.connected==false">
-        <h1>You have to be login</h1>
-    </v-container>
-    <v-container class="pa-4 text-center" v-if="this.$store.state.connected==true">
-        <h1 class="grey--text m-12">Select the playlist you want to create</h1>
-        <v-row class="fill-height" align="center" justify="center">
-            <template v-for="(item, i) in info">
-                {{ test }}
-                <v-col :key="i" cols="12" md="4">
-                    <v-hover>
-                        <v-card
-                            slot-scope="{ hover }"
-                            :elevation="hover ? 12 : 2"
-                            :class="{ 'on-hover': hover }"
-                        >
-                            <v-img
-                                @click="choose(item.name)"
-                                @click.stop="dialog = true"
-                                :src="item.img"
-                                height="225px"
+    <div>
+        <v-container class="pa-4 text-center" v-if="this.$store.state.connected==false">
+            <h1>You have to be login</h1>
+        </v-container>
+        <v-container class="pa-4 text-center" v-if="this.$store.state.connected==true">
+            <h1 class="grey--text m-12">Select the playlist you want to create</h1>
+            <v-row class="fill-height" align="center" justify="center">
+                <template v-for="(item, i) in info">
+                    {{ test }}
+                    <v-col :key="i" cols="12" md="4">
+                        <v-hover>
+                            <v-card
+                                slot-scope="{ hover }"
+                                :elevation="hover ? 12 : 2"
+                                :class="{ 'on-hover': hover }"
                             >
-                                <v-card-title class="title white--text">
-                                    <v-row class="fill-height flex-column" justify="space-between">
-                                        <p
-                                            class="mt-4 subheading text-left text-uppercase"
-                                        >{{ item.name }}</p>
-                                        <div>
-                                            <p
-                                                class="ma-0 body-1 font-weight-bold font-italic text-left"
-                                            >Based on your {{ item.name }} loved tracks</p>
-                                            <p
-                                                class="caption font-weight-medium font-italic text-left"
-                                            ></p>
-                                        </div>
-                                    </v-row>
-                                </v-card-title>
-                                <v-btn
-                                    icon
-                                    class="{ 'show-btns': hover }"
-                                    color="transparent"
-                                    v-bind:style="{cursor: pointer}"
+                                <v-img
+                                    @click="choose(item.id)"
+                                    @click.stop="dialog = true"
+                                    :src="item.img"
+                                    height="225px"
                                 >
-                                    <v-icon :class="{ 'show-btns': hover }" color="transparent">add</v-icon>
-                                </v-btn>
-                            </v-img>
-                        </v-card>
-                    </v-hover>
-                </v-col>
-            </template>
-        </v-row>
-        <div class="dialog">
-            <v-row justify="center">
-                <v-dialog content-class="v-dialog" v-model="dialog" max-width="290">
-                    <div class="dialog">
-                        <v-card-title class="headline">Warning</v-card-title>
-
-                        <v-card-text>Are you sure to want to create {{ chosen_playlist }} playlist ?</v-card-text>
-
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-
-                            <v-btn color="primary" text @click="dialog = false">Disagree</v-btn>
-
-                            <v-btn color="primary" text @click="dialog = false">Agree</v-btn>
-                        </v-card-actions>
-                    </div>
-                </v-dialog>
+                                    <v-card-title class="title white--text">
+                                        <v-row
+                                            class="fill-height flex-column"
+                                            justify="space-between"
+                                        >
+                                            <p
+                                                class="mt-4 subheading text-left text-uppercase"
+                                            >{{ item.name }}</p>
+                                            <div>
+                                                <p
+                                                    class="ma-0 body-1 font-weight-bold font-italic text-left"
+                                                >Based on your {{ item.name }} loved tracks</p>
+                                                <p
+                                                    class="caption font-weight-medium font-italic text-left"
+                                                ></p>
+                                            </div>
+                                        </v-row>
+                                    </v-card-title>
+                                    <v-btn
+                                        icon
+                                        class="{ 'show-btns': hover }"
+                                        color="transparent"
+                                        v-bind:style="{cursor: pointer}"
+                                    >
+                                        <v-icon
+                                            :class="{ 'show-btns': hover }"
+                                            color="transparent"
+                                        >add</v-icon>
+                                    </v-btn>
+                                </v-img>
+                            </v-card>
+                        </v-hover>
+                    </v-col>
+                </template>
             </v-row>
-        </div>
-    </v-container>
-</div>
+            <div class="dialog">
+                <v-row justify="center">
+                    <v-dialog content-class="v-dialog" v-model="dialog" max-width="290">
+                        <div class="dialog">
+                            <v-card-title class="headline">Warning</v-card-title>
+
+                            <v-card-text>Are you sure to want to create {{ chosen_playlist }} playlist ?</v-card-text>
+
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+
+                                <v-btn color="primary" text @click="dialog = false">Disagree</v-btn>
+
+                                <v-btn
+                                    color="primary"
+                                    text
+                                    @click="createPlaylist(chosen_playlist), dialog=false"
+                                >Agree</v-btn>
+                            </v-card-actions>
+                        </div>
+                    </v-dialog>
+                </v-row>
+            </div>
+        </v-container>
+    </div>
 </template>
 
 <script>
@@ -105,15 +115,14 @@ export default {
             headers: {
                 jwt_token: this.$store.state.jwt_token
             }
-        }
+        };
         axios
             .get("http://localhost:5000/get-suggest-playlist", config)
             .then(
                 response =>
                     (this.info = this.add_url(
                         response["data"]["relevant_category"]
-                    )
-                    )
+                    ))
             );
     },
     methods: {
@@ -124,7 +133,34 @@ export default {
             return response;
         },
         choose(chosen_playlist) {
+            console.log(chosen_playlist);
             this.chosen_playlist = chosen_playlist;
+        },
+        createPlaylist(chosen_playlist) {
+            let config = {
+                headers: {
+                    jwt_token: this.$store.state.jwt_token
+                }
+            };
+            var bodyFormData = new FormData();
+            bodyFormData.set("category_id", chosen_playlist);
+
+            axios({
+                method: "post",
+                url: "http://localhost:5000/create-playlist",
+                data: bodyFormData,
+                headers: {
+                    jwt_token: this.$store.state.jwt_token
+                }
+            })
+                .then(function(response) {
+                    //handle success
+                    console.log(response);
+                })
+                .catch(function(response) {
+                    //handle error
+                    console.log(response);
+                });
         }
     }
 };
