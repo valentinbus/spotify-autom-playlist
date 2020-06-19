0662f4702b1bc2c76fe11d3d3dc16499a4bba9fb
Valentin BUS <valentinoiho@gmail.com>
2020-06-19 11:07:51 +0200

    [unittest] add all app tests

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -15,7 +15,6 @@ from app.backend.models import User
 from pprint import pprint
 
 
-
 class TestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
@@ -40,7 +39,6 @@ class TestCase(unittest.TestCase):
         assert response.status_code == 200
         self.assertEqual(url, b'"authorization_url"\n')
 
-
     @mock.patch('__main__.Spotify._get_user_id')
     def test_get_token(self, mock_spotify):
         mock_spotify.return_value = 'valentinoiho'
@@ -49,7 +47,6 @@ class TestCase(unittest.TestCase):
 
         assert response.status_code == 200
         self.assertEqual(response.get_data(), b'{"jwt_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVyX3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-m3zkpWzefzzAyimfpKHDj5gaNzgP2Q"}\n')
-
 
     @mock.patch("__main__.Spotify._check_token")
     def test_check_token(self, mock_check_token):
@@ -63,17 +60,22 @@ class TestCase(unittest.TestCase):
         )
 
         assert response.status_code == 200
-        self.assertEqual(response.get_data(), b'{"error": "Not enough segments:::Check Jwt token"}\n')
+        self.assertEqual(
+            response.get_data(),
+            b'{"error": "Not enough segments:::Check Jwt token"}\n'
+        )
 
         response = self.app.get(
             'check-token',
             headers={
-                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVyX3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-m3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
+                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+                '.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVy'
+                'X3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-'
+                'm3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
             }
         )
 
         self.assertEqual(response.get_data(), b'"JWT TOKEN"\n')
-
 
     @mock.patch('__main__.Spotify.init_db')
     def test_init_db(self, mock_spotify):
@@ -83,13 +85,15 @@ class TestCase(unittest.TestCase):
         response = self.app.put(
             'init-db',
             headers={
-                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVyX3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-m3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
+                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+                '.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVy'
+                'X3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-'
+                'm3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
             }
         )
 
         assert response.status_code == 200
         self.assertEqual(response.get_data(), b'"response ok"\n')
-
 
     @mock.patch("__main__.Spotify.clear_db")
     def test_clear_db(self, mock_spotify):
@@ -98,13 +102,15 @@ class TestCase(unittest.TestCase):
         response = self.app.delete(
             'clear-db',
             headers={
-                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVyX3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-m3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
+                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+                '.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVy'
+                'X3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-'
+                'm3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
             }
         )
 
         assert response.status_code == 200
         self.assertEqual(response.get_data(), b'"response ok"\n')
-
 
     @mock.patch("__main__.Spotify.get_tracks")
     def test_get_tracks(self, mock_spotify):
@@ -113,13 +119,15 @@ class TestCase(unittest.TestCase):
         response = self.app.get(
             'get-tracks',
             headers={
-                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVyX3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-m3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
+                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+                '.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVy'
+                'X3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-'
+                'm3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
             }
         )
 
         assert response.status_code == 200
         self.assertEqual(response.get_data(), b'"response ok"\n')
-
 
     @mock.patch("__main__.Spotify.get_playlist")
     def test_get_playlist(self, mock_spotify):
@@ -128,13 +136,15 @@ class TestCase(unittest.TestCase):
         response = self.app.get(
             'get-playlist',
             headers={
-                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVyX3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-m3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
+                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+                '.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVy'
+                'X3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-'
+                'm3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
             }
         )
 
         assert response.status_code == 200
         self.assertEqual(response.get_data(), b'"response ok"\n')
-
 
     @mock.patch("__main__.Spotify.get_user")
     def test_get_user(self, mock_spotify):
@@ -143,13 +153,35 @@ class TestCase(unittest.TestCase):
         response = self.app.get(
             'get-user',
             headers={
-                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVyX3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-m3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
+                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+                '.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVy'
+                'X3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-'
+                'm3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
             }
         )
 
         assert response.status_code == 200
         self.assertEqual(response.get_data(), b'"response ok"\n')
 
+    @mock.patch("__main__.Spotify.create_playlist")
+    def test_get_create_playlist(self, mock_spotify):
+        mock_spotify.return_value = "response ok"
+
+        response = self.app.post(
+            'create-playlist',
+            headers={
+                'jwt_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+                '.eyJ1c2VyX2lkIjoidmFsZW50aW5vaWhvIiwiYmFlcmVy'
+                'X3Rva2VuIjoiQmVhcmVyIE5vbmUifQ.j_0jkZGXA2Vv-'
+                'm3zkpWzefzzAyimfpKHDj5gaNzgP2Q'
+            },
+            data={
+                'category_id': 'none'
+            }
+        )
+
+        assert response.status_code == 200
+        self.assertEqual(response.get_data(), b'"response ok"\n')
 
 
 if __name__ == '__main__':
